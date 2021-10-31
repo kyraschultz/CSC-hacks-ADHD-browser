5ee559058d409126c0bcb442e5ce42675a8b4518
skb4996 <skb49@pitt.edu>
2021-10-31 19:49:28 +0000

    added to popup view and functionality

--- popup.js
+++ popup.js
@@ -1,6 +1,39 @@
-//initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
+//An array of puppy images. 0th index 
+//is a pic with zero the dog, 6th index
+//has a pic with multiple puppies
 
-chrome.storage.sync.get("color", ({color}) => {
-  changeColor.style.backgroundColor = color;
-});
+let dogImages = [ 
+"https://images.app.goo.gl/YuUJb3yHBBqeJTDd9",
+"https://images.app.goo.gl/JsiAFP7g1Fby2QyK9",
+"https://images.app.goo.gl/kXj9d7q2dqNmToJJ7",
+"https://images.app.goo.gl/AwwWh5qdZPECMaQn7",
+"https://images.app.goo.gl/mtx7NnW5cViQPJYA8",
+"https://images.app.goo.gl/zthP1MjpgDsFZTk98",
+"https://images.app.goo.gl/EUDQP4wigFzk5DSP9"
+];
+
+
+//Definition of a task that a user submits
+
+var numberOfTasks = 0;
+
+let listOfTasks = [];
+//key
+//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
+
+function setDogPic() {
+    const dogTaskPic = document.getElementById("dogPicture")
+    
+    if(numberOfTasks > 0) {
+        dogTaskPic = dogImages[numberOfTasks]
+    }
+};
+
+ let addNewTask =(ev)=> {
+  ev.preventDefault;
+  let task = {
+    id: Date.new(),
+    task_Description: document.getElementById("task_Description").value,
+    
+  }
+}
